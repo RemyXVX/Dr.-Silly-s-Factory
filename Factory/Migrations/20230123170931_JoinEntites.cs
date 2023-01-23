970a3652682a9a8413b271b35882fad465e71f68
RemyXVX <remyfranciscoflores@gmail.com>
2023-01-23 09:11:42 -0800

    Updated migrations

---
+++ Factory/Migrations/20230123170931_JoinEntites.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class JoinEntites : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Engineers_Machines_MachineId",
+                table: "Engineers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Engineers_MachineId",
+                table: "Engineers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Engineers_MachineId",
+                table: "Engineers",
+                column: "MachineId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Engineers_Machines_MachineId",
+                table: "Engineers",
+                column: "MachineId",
+                principalTable: "Machines",
+                principalColumn: "MachineId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
